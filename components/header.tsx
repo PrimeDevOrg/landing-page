f5886b16f120572b11e523b049658d02482a45e7
Davi Rezende <daviresendes12@gmail.com>
2025-07-31 13:01:44 -0300

    feat: restyle hero and header

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,107 @@
-import { Menu } from 'lucide-react';
-import Image from 'next/image';
-import Logo from '../app/assets/logo.svg';
-import { Button } from './ui/button';
+import { Menu } from 'lucide-react'
+import Image from 'next/image'
+import { useState } from 'react'
+import Logo from '../app/assets/logo.svg'
+import { Button } from './ui/button'
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from './ui/dropdown-menu';
+} from './ui/dropdown-menu'
+import {
+  MobileNav,
+  MobileNavHeader,
+  MobileNavMenu,
+  MobileNavToggle,
+  Navbar,
+  NavbarButton,
+  NavBody,
+  NavItems
+} from './ui/resizable-navbar'
 
 export function Header() {
+  const navItems = [
+    {
+      name: 'Serviços',
+      link: '#services',
+    },
+    {
+      name: 'Sobre nós',
+      link: '#about',
+    },
+    {
+      name: 'Depoimentos',
+      link: '#testimonials',
+    },
+    {
+      name: 'FAQ',
+      link: '#faq',
+    },
+    {
+      name: 'Contato',
+      link: '#contact',
+    },
+  ]
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+  return (
+    <div className="fixed z-50 w-full">
+      <Navbar>
+        {/* Desktop Navigation */}
+        <NavBody>
+          <Image src={Logo} alt="PrimeDev" width={140} />
+          <NavItems items={navItems} className="text-white" />
+          <NavbarButton className="flex items-center gap-4" variant="primary">
+            <a
+              href="https://wa.me/+5562982559125?text=Ol%C3%A1%2C+estou+interessado+em+um+dos+servi%C3%A7os+da+PrimeDev%2C+pode+me+falar+mais+sobre+isso%3F"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Solicitar orçamento
+            </a>
+          </NavbarButton>
+        </NavBody>
+
+        {/* Mobile Navigation */}
+        <MobileNav>
+          <MobileNavHeader>
+            <Image src={Logo} alt="PrimeDev" width={140} />
+            <MobileNavToggle
+              isOpen={isMobileMenuOpen}
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            />
+          </MobileNavHeader>
+
+          <MobileNavMenu
+            isOpen={isMobileMenuOpen}
+            onClose={() => setIsMobileMenuOpen(false)}
+          >
+            {navItems.map((item, idx) => (
+              <a
+                key={`mobile-link-${idx}`}
+                href={item.link}
+                onClick={() => setIsMobileMenuOpen(false)}
+                className="relative text-neutral-600 dark:text-neutral-300"
+              >
+                <span className="block">{item.name}</span>
+              </a>
+            ))}
+            <a
+              href="https://wa.me/+5562982559125?text=Ol%C3%A1%2C+estou+interessado+em+um+dos+servi%C3%A7os+da+PrimeDev%2C+pode+me+falar+mais+sobre+isso%3F"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Button>Solicitar orçamento</Button>
+            </a>
+          </MobileNavMenu>
+        </MobileNav>
+      </Navbar>
+    </div>
+  )
+}
+
+export function Header_() {
   return (
     <div className="fixed left-1/2 z-[1000] w-full max-w-[1026px] -translate-x-1/2 md:top-4 lg:top-6">
       <header className="hidden w-full items-center justify-between gap-4 rounded-full border bg-white px-3 py-2 shadow backdrop-blur-sm lg:flex">
@@ -71,5 +163,5 @@ export function Header() {
         </a>
       </header>
     </div>
-  );
+  )
 }
--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,132 +1,23 @@
-'use client';
-import { Button } from '../ui/button';
+'use client'
+import { Button } from '../ui/button'
+import { ContainerTextFlip } from '../ui/container-text-flip'
 
-import MotionPathPlugin from 'gsap/MotionPathPlugin';
-import ScrollToPlugin from 'gsap/ScrollToPlugin';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
-import { useRef } from 'react';
-import FadeContent from '../ui/FadeContent/FadeContent';
-import Silk from '../ui/Silk/Silk';
-import { TextGenerateEffect } from '../ui/text-generate-effect';
-gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollToPlugin, MotionPathPlugin);
+import FadeContent from '../ui/FadeContent/FadeContent'
+import { GridBackground } from '../ui/grid-background'
 
 export function Hero() {
-  const container = useRef<HTMLDivElement | null>(null);
-
-  useGSAP(
-    () => {
-      const tl = gsap.timeline();
-      const end = '+=600';
-      tl.to('.box-zap', {
-        scrollTrigger: {
-          trigger: container.current,
-          scrub: true,
-          start: 'top top',
-          end,
-        },
-        ease: 'sine.out',
-        motionPath: {
-          path: '.mp-zap',
-          align: '.mp-zap',
-          alignOrigin: [0.5, 0.5],
-        },
-      });
-
-      tl.to('.box-gear', {
-        scrollTrigger: {
-          trigger: container.current,
-          scrub: true,
-          start: 'top top',
-          end,
-        },
-        ease: 'sine.out',
-        motionPath: {
-          path: '.mp-gear',
-          align: '.mp-gear',
-          alignOrigin: [0.5, 0.5],
-        },
-      });
-      tl.to('.box-light', {
-        scrollTrigger: {
-          trigger: container.current,
-          scrub: true,
-          start: 'top top',
-          end,
-        },
-        ease: 'sine.out',
-        motionPath: {
-          path: '.mp-light',
-          align: '.mp-light',
-          alignOrigin: [0.5, 0.5],
-        },
-      });
-      tl.to('.box-code', {
-        scrollTrigger: {
-          trigger: container.current,
-          scrub: true,
-          start: 'top top',
-          end,
-        },
-        ease: 'sine.out',
-        motionPath: {
-          path: '.mp-code',
-          align: '.mp-code',
-          alignOrigin: [0.5, 0.5],
-        },
-      });
-      tl.to('.box-security', {
-        scrollTrigger: {
-          trigger: container.current,
-          scrub: true,
-          start: 'top top',
-          end,
-        },
-        ease: 'sine.out',
-        motionPath: {
-          path: '.mp-security',
-          align: '.mp-security',
-          alignOrigin: [0.5, 0.5],
-        },
-      });
-      tl.to('.box-server', {
-        scrollTrigger: {
-          trigger: container.current,
-          scrub: true,
-          start: 'top top',
-          end,
-        },
-        ease: 'sine.out',
-        motionPath: {
-          path: '.mp-server',
-          align: '.mp-server',
-          alignOrigin: [0.5, 0.5],
-        },
-      });
-    },
-    { scope: container },
-  );
   return (
     <section
       id="home"
-      className="relative flex h-screen flex-col items-center justify-center overflow-hidden border-b border-slate-300 bg-white"
-      ref={container}
+      className="relative flex h-screen flex-col items-center justify-center overflow-hidden border-b border-slate-300 bg-zinc-950"
     >
-      <Silk
-        speed={5}
-        scale={1}
-        color="#ea580c"
-        noiseIntensity={0.5}
-        rotation={0}
-      />
-
-      <div className="absolute z-40 flex max-w-[1026px] flex-col items-center justify-center gap-3 p-8">
-        <h1 className="max-w-[960px] text-center">
-          <TextGenerateEffect
-            words="Criamos softwares que transformam negócios"
-            className="text-white"
+      <GridBackground />
+      <div className="absolute z-40 flex max-w-[1026px] flex-col justify-center gap-3 p-8">
+        <h1 className="max-w-[960px] text-center text-4xl font-bold text-zinc-950 md:text-7xl">
+          Sua empresa precisa de um software{' '}
+          <ContainerTextFlip
+            className="bg-slate-700"
+            words={['rápido', 'moderno', 'seguro', 'escalável']}
           />
         </h1>
 
@@ -135,28 +26,21 @@ export function Hero() {
           duration={500}
           easing="ease-out"
           initialOpacity={0}
+          className="flex flex-col items-center gap-4"
         >
-          <p className="text-center text-sm font-medium text-white md:text-base lg:text-lg">
-            Na PrimeDev, desenvolvemos websites, apps móveis e APIs com{' '}
-            <span className="text-nowrap rounded bg-brand px-3 text-white">
-              qualidade excepcional
-            </span>{' '}
-            e desempenho incomparável. Transforme suas ideias em realidade com{' '}
-            <span className="text-nowrap rounded bg-brand px-3 text-white">
-              tecnologia de ponta
-            </span>
-            .
+          <p className="text-center text-lg font-medium text-zinc-700 md:text-base lg:text-xl">
+            Criamos softwares personalizados para transformar ideias em soluções
+            digitais.
           </p>
+          <a
+            href="https://wa.me/+5562982559125?text=Ol%C3%A1%2C+estou+interessado+em+um+dos+servi%C3%A7os+da+PrimeDev%2C+pode+me+falar+mais+sobre+isso%3F"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button size="lg">Entrar em contato</Button>
+          </a>
         </FadeContent>
-
-        <a
-          href="https://wa.me/+5562982559125?text=Ol%C3%A1%2C+estou+interessado+em+um+dos+servi%C3%A7os+da+PrimeDev%2C+pode+me+falar+mais+sobre+isso%3F"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Button size="lg">Entrar em contato</Button>
-        </a>
       </div>
     </section>
-  );
+  )
 }
